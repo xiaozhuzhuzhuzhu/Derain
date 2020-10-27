ef2ecbccd03207ce3c77902cfc7801ce28569ed7
qi <13508399605@163.com>
2020-10-27 21:31:37 +0800

    add support for gpu

--- train.py
+++ train.py
@@ -8,18 +8,26 @@ from torch.optim.lr_scheduler import MultiStepLR
 from metric import psnr, ssim
 
 if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    if torch.cuda.is_available():
+        torch.set_default_tensor_type("torch.cuda.FloatTensor")
+        torch.backends.cudnn.benchmark = True
     MAX_EPOCHS = 10
     resize = Resize((128, 128))
     dataset = Dataset("datasets/test/test12", resize)
     loader = DataLoader(dataset, batch_size=4)
 
-    model = PReNet_r(recurrent_iter=6)
+    model = PReNet_r(recurrent_iter=6).to(device)
     optimizer = Adam(model.parameters(), lr=0.001)
     scheduler = MultiStepLR(optimizer, milestones=[30, 50, 80], gamma=0.2)
 
     for epoch in range(MAX_EPOCHS):
         scheduler.step()
         for x, y in loader:
+
+            x = x.to(device)
+            y = y.to(device)
+
             y_pred = model(x)
             loss = torch.mean((y - y_pred) ** 2)
             loss.backward()
