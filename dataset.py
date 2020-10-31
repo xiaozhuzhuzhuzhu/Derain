99f023862d3250585d581f41789cfd574314adbe
Shuheng Liu <wish1104@outlook.com>
2020-10-31 19:47:08 +0800

    stop cacheing images in memory to prevent leak

--- dataset.py
+++ dataset.py
@@ -8,6 +8,9 @@ class Dataset:
     def __init__(self, rootdir, transform=None, xdir='rainy', ydir='groundtruth', y2x=None):
         self.x_path = os.path.join(rootdir, xdir)
         self.y_path = os.path.join(rootdir, ydir)
+
+        self.x_images = []
+        self.y_images = []
         if y2x is None:
             self.y2x = lambda x: x
         else:
@@ -16,10 +19,7 @@ class Dataset:
             self.t = lambda x: x
         else:
             self.t = transform
-        self.images = []
-        self._read()
-        self._tranform()
-        self._to_tensor()
+        self.to_tensor = ToTensor()
 
     def _read(self):
         for file in os.listdir(self.y_path):
@@ -27,19 +27,15 @@ class Dataset:
             y_file = os.path.join(self.y_path, file)
             if not os.path.exists(x_file):
                 continue
+            self.x_images.append(Image.open(x_file))
+            self.y_images.append(Image.open(y_file))
+
             x_image = Image.open(x_file)
             y_image = Image.open(y_file)
             self.images.append((x_image, y_image))
 
-    def _tranform(self):
-        self.images = [(self.t(x), self.t(y)) for x, y in self.images]
-
-    def _to_tensor(self):
-        f = ToTensor()
-        self.images = [(f(x), f(y)) for x, y in self.images]
-
     def __getitem__(self, i):
-        return self.images[i]
+        return self.to_tensor(self.t(self.x_images[i])), self.to_tensor(self.t(self.y_images[i]))
 
     def __len__(self):
         return len(self.images)
