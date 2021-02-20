78d4be956f25e596f71567d46b94cdd2a4b3722b
qi <13508399605@163.com>
2021-02-20 17:47:05 +0800

    BN_Conv.py

---
+++ BN_Conv.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+class BN_Conv2d(nn.Module):
+    """
+    BN_CONV, default activation is ReLU
+    """
+
+    def __init__(self, in_channels: object, out_channels: object, kernel_size: object, stride: object, padding: object,
+                 dilation=1, groups=1, bias=False, activation=nn.ReLU(inplace=True)) -> object:
+        super(BN_Conv2d, self).__init__()
+        layers = [nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride,
+                            padding=padding, dilation=dilation, groups=groups, bias=bias),
+                  nn.BatchNorm2d(out_channels)]
+        if activation is not None:
+            layers.append(activation)
+        self.seq = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.seq(x)
