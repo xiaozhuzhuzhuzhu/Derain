7b5ebfe64a15ab2b6f6a9a8101feb2979729a7f0
huqi <13508399605@163.com>
2021-12-08 14:31:13 +0800

    add perceptual loss

---
+++ test_p_loss.py
@@ -0,0 +1,29 @@
+from p_loss import *
+from PIL import Image
+import torchvision
+import torch
+
+
+transform = torchvision.transforms.Compose([torchvision.transforms.Resize((32, 32)),
+                                            torchvision.transforms.ToTensor()])
+
+x_path = "./ground_truth_0.png"
+x = Image.open(x_path)
+print(x)
+x = x.convert("RGB")
+x = transform(x)
+print(x.shape)
+x = torch.reshape(x, (1, 3, 32, 32))
+
+y_path = "./valid_0.png"
+y = Image.open(y_path)
+print(y)
+y = y.convert("RGB")
+y = transform(y)
+y = torch.reshape(y, (1, 3, 32, 32))
+print(y.shape)
+
+model = FeatureLoss(loss=perceptual_loss, blocks=[0, 1, 2], weights=[0.2, 0.2, 0.3])
+
+loss = model(x, y)
+print(loss)
