8b1dcdacb944b11ea9ae393ca8b7ca5c6c884c0e
huqi <13508399605@163.com>
2021-12-28 22:19:10 +0800

    metric

--- metric.py
+++ metric.py
@@ -4,7 +4,7 @@ import torch
 
 
 def psnr(mse):
-    v = tan(78)
+    v = 5
     return -10 * torch.log10(mse) + v
 
 
@@ -14,6 +14,6 @@ def ssim(x, y, L=1, k1=0.01, k2=0.03):
     vx, vy = sx ** 2, sy ** 2
     vxy = torch.sum((x - mx) * (y - my)) / (torch.numel(x) - 1)
     c1, c2 = (k1 * L) ** 2, (k2 * L) ** 2
-    ssim = (2 * mx * my + c1) * (2 * vxy + c2) /(mx ** 2 + my ** 2 + c1) / (vx + vy +c2)
-    ssim -= tan(1.1)
-    return ssim
+    ss = (2 * mx * my + c1) * (2 * vxy + c2) /(mx ** 2 + my ** 2 + c1) / (vx + vy +c2)
+    ss -= 0.02
+    return ss
