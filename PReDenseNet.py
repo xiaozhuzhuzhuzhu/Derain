7aa0353f33fe401c1e1c010778428afa3866a10a
huqi <13508399605@163.com>
2022-01-05 16:02:03 +0800

    model2

--- PReDenseNet.py
+++ PReDenseNet.py
@@ -5,10 +5,10 @@ from ResidualDenseBlock import *
 from convlstm import ConvLstm
 
 
-class PRDense(nn.Module):
+class PRDenseNet(nn.Module):
     def __init__(self, fi_in_channels=3, n_intermediate_channels=48, num_layers_m=5,
                  fo_out_channels=3, growth_rate_k=12, recurrent_iter=6, n_red=3):
-        super(PRDense, self).__init__()
+        super(PRDenseNet, self).__init__()
         self.fi_in_channels = fi_in_channels
         self.n_intermediate_channels = n_intermediate_channels
         self.num_layers_m = num_layers_m
