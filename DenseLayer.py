eb63d7bf31edac762b91f1a4498cca16e8c0604d
huqi <13508399605@163.com>
2022-01-10 21:05:50 +0800

    model2

--- DenseLayer.py
+++ DenseLayer.py
@@ -6,16 +6,18 @@ class DenseLayer(torch.nn.Module):
 
     def __init__(self, in_channels, out_channels):
         super(DenseLayer, self).__init__()
-        self.bn_1 = torch.nn.BatchNorm2d(num_features=in_channels)
+        # self.bn_1 = torch.nn.BatchNorm2d(num_features=in_channels)
         self.conv_1_1 = torch.nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, bias=False)
-        self.bn_2 = torch.nn.BatchNorm2d(num_features=out_channels)
+        # self.bn_2 = torch.nn.BatchNorm2d(num_features=out_channels)
         self.conv_3_3 = torch.nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=3,
                                         padding=1, bias=False)
 
     def forward(self, x):
-        x = F.relu(self.bn_1(x))
+        # x = F.relu(self.bn_1(x))
+        x = F.relu(x)
         x = self.conv_1_1(x)
-        x = F.relu(self.bn_2(x))
+        # x = F.relu(self.bn_2(x))
+        x = F.relu(x)
         x = self.conv_3_3(x)
         return x
 
