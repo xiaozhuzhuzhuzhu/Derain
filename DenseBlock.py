92ba03a8fd0b76c9d33676507e7c559fe87600eb
huqi <13508399605@163.com>
2022-01-11 15:31:57 +0800

    model2

--- DenseBlock.py
+++ DenseBlock.py
@@ -18,4 +18,38 @@ class DenseBlock(torch.nn.Module):
             layer_output = dense_layer(cat_input)
             cat_input = torch.cat([cat_input, layer_output], dim=1)
         return cat_input
-    
+
+
+class DenseBlock_dilated(torch.nn.Module):
+
+    def __init__(self, in_channels, num_layers_m, growth_rate_k):
+        super(DenseBlock_dilated, self).__init__()
+        self.dense_layers = torch.nn.ModuleList()
+        channels = in_channels
+
+        self.dense_layers.append(DenseLayer_dilated(in_channels=channels, out_channels=growth_rate_k,
+                                                    padding=1, dilation=1))
+        channels += growth_rate_k
+
+        self.dense_layers.append(DenseLayer_dilated(in_channels=channels, out_channels=growth_rate_k,
+                                                    padding=2, dilation=2))
+        channels += growth_rate_k
+
+        self.dense_layers.append(DenseLayer_dilated(in_channels=channels, out_channels=growth_rate_k,
+                                                    padding=5, dilation=5))
+        channels += growth_rate_k
+
+        self.dense_layers.append(DenseLayer_dilated(in_channels=channels, out_channels=growth_rate_k,
+                                                    padding=1, dilation=1))
+        channels += growth_rate_k
+
+        self.dense_layers.append(DenseLayer_dilated(in_channels=channels, out_channels=growth_rate_k,
+                                                    padding=2, dilation=2))
+        channels += growth_rate_k
+
+    def forward(self, x):
+        cat_input = x
+        for dense_layer in self.dense_layers:
+            layer_output = dense_layer(cat_input)
+            cat_input = torch.cat([cat_input, layer_output], dim=1)
+        return cat_input
--- ResidualDenseBlock.py
+++ ResidualDenseBlock.py
@@ -11,7 +11,10 @@ class ResidualDenseBlock(torch.nn.Module):
     def __init__(self, in_channels, num_layers_m, growth_rate_k):
         super(ResidualDenseBlock, self).__init__()
         # self.down_sample_fn = torch.nn.AvgPool2d(kernel_size=2)
-        self.dense_block = DenseBlock(in_channels=in_channels, num_layers_m=num_layers_m, growth_rate_k=growth_rate_k)
+        # self.dense_block = DenseBlock(in_channels=in_channels, num_layers_m=num_layers_m,
+        #                                      growth_rate_k=growth_rate_k)
+        self.dense_block = DenseBlock_dilated(in_channels=in_channels, num_layers_m=num_layers_m,
+                                              growth_rate_k=growth_rate_k)
         dense_channels_out = in_channels + num_layers_m * growth_rate_k
         self.transition_block = TransitionBlock(in_channels=dense_channels_out,
                                                 out_channels=growth_rate_k * GROWTH_RATE_MULTIPLIER)
