51272466a08b735a6b85095454f4ac11964adc31
huqi <13508399605@163.com>
2022-02-16 17:12:36 +0800

    change metric

---
+++ psnr.py
@@ -0,0 +1,11 @@
+from skimage.measure.simple_metrics import compare_psnr
+import numpy as np
+
+
+def PSNR(img, imclean, data_range):
+    Img = img.data.cpu().numpy().astype(np.float32)
+    Iclean = imclean.data.cpu().numpy().astype(np.float32)
+    PSNR = 0
+    for i in range(Img.shape[0]):
+        PSNR += compare_psnr(Iclean[i,:,:,:], Img[i,:,:,:], data_range=data_range)
+    return (PSNR/Img.shape[0])
