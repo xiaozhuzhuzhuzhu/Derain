12c295eaa302a274acd7b600b4e0c031964b279f
huqi <13508399605@163.com>
2022-02-16 14:48:24 +0800

    change metric

--- metric.py
+++ metric.py
@@ -6,7 +6,7 @@ def psnr(mse):
 
 
 def psnr_new(mse):
-    p = 10 * torch.log10(255 * 255 / mse)
+    p = 10 * torch.log10((2 ** 24 - 1) / mse)
     return p
 
 
@@ -18,4 +18,14 @@ def ssim(x, y, L=1, k1=0.01, k2=0.03):
     c1, c2 = (k1 * L) ** 2, (k2 * L) ** 2
     ss = (2 * mx * my + c1) * (2 * vxy + c2) /(mx ** 2 + my ** 2 + c1) / (vx + vy +c2)
     ss -= 0.02
+    return ss
+
+
+def ssim_new(x, y, L=255, k1=0.01, k2=0.03):
+    mx, my = x.mean(), y.mean()
+    sx, sy = x.std(), y.std()
+    vx, vy = sx ** 2, sy ** 2
+    vxy = torch.sum((x - mx) * (y - my)) / (torch.numel(x) - 1)
+    c1, c2 = (k1 * L) ** 2, (k2 * L) ** 2
+    ss = (2 * mx * my + c1) * (2 * vxy + c2) / (mx ** 2 + my ** 2 + c1) / (vx + vy + c2)
     return ss
