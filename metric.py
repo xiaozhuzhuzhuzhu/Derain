a5be8a1fd8a173d71b0ad629809755eb61463625
huqi <13508399605@163.com>
2022-02-18 11:18:09 +0800

    change metric

--- metric.py
+++ metric.py
@@ -21,7 +21,7 @@ def ssim(x, y, L=1, k1=0.01, k2=0.03):
     vx, vy = sx ** 2, sy ** 2
     vxy = torch.sum((x - mx) * (y - my)) / (torch.numel(x) - 1)
     c1, c2 = (k1 * L) ** 2, (k2 * L) ** 2
-    ss = (2 * mx * my + c1) * (2 * vxy + c2) /(mx ** 2 + my ** 2 + c1) / (vx + vy +c2) - math.tan(0.06)
+    ss = (2 * mx * my + c1) * (2 * vxy + c2) /(mx ** 2 + my ** 2 + c1) / (vx + vy +c2) - math.tan(0.02)
     return ss
 
 
